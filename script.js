a722afa60efc3b39621b46a73f1ca0ea84bb4bfb
Ibenholter <ibenholter@outlook.com>
2025-02-25 08:24:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ function tegnLinje(startx, starty, stopx, stopy, farge) {
     ctx.stroke();
 }
 
-tegnLinje(WIDTH / 2, 0, WIDTH / 2, HEIGHT, "black")
-tegnLinje(WIDTH / 2, 0, WIDTH, 0, "blak")
-tegnLinje(0, 500, WIDTH, HEIGHT, "black")
-tegnLinje(WIDTH, 0, WIDTH, HEIGHT / 5, "black")
-tegnLinje(WIDTH / 2, HEIGHT / 6, WIDTH / 1.5, 0, "black");
+const offset = 50
+
+tegnLinje(WIDTH / 2 - offset, 0, WIDTH / 2 - offset, HEIGHT, "black")
+tegnLinje(WIDTH / 2 - offset, 0, WIDTH - offset, 0, "blak")
+tegnLinje(WIDTH - offset, 0, WIDTH - offset, HEIGHT / 5, "black")
+tegnLinje(WIDTH / 2 - offset, HEIGHT / 6, WIDTH / 1.5 - offset, 0, "black");
+
+
 
 
 
